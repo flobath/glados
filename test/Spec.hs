c1497d0daffc21bb6e62234976b300279d367d16
Florent Charpentier <florent.charpentier@epitech.eu>
2024-11-29 02:58:18 +0100

    test(parser): Add tests for boolean parser

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,26 @@
+import Test.Hspec.Megaparsec (shouldParse, succeedsLeaving, initialState, failsLeaving)
+import Test.Hspec (hspec, describe, it)
+import Text.Megaparsec (parse, runParser')
+import Parser (booleanParser)
+import Lib (Primitive(Boolean))
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = hspec $
+    describe "parseBoolean" $ do
+        it "parse a simple #t" $
+            parse booleanParser "" "#t" `shouldParse` Boolean True
+        it "parse a simple #f" $
+            parse booleanParser "" "#f" `shouldParse` Boolean False
+
+        it "consume trailing whitespace" $
+            runParser' booleanParser (initialState "#t  \n   \t(abc") `succeedsLeaving` "(abc"
+
+        it "fail with #fabcd" $
+            runParser' booleanParser (initialState "#fabcd") `failsLeaving` "#fabcd"
+
+        it "stops at opening parenthesis" $
+            runParser' booleanParser (initialState "#f(hi)") `succeedsLeaving` "(hi)"
+        it "stops at opening quote" $
+            runParser' booleanParser (initialState "#f\"hi\"") `succeedsLeaving` "\"hi\""
+        it "stops at following boolean literal" $
+            runParser' booleanParser (initialState "#f#t") `succeedsLeaving` "#t"
