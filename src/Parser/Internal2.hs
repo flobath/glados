1324702eb5e1eefd6a962f93b3f791c4bd41b05a
Florent Charpentier <florent.charpentier@epitech.eu>
2024-12-30 23:53:57 +0100

    feat(parser): Start parser 2.0

---
+++ src/Parser/Internal2.hs
@@ -0,0 +1,69 @@
+module Parser.Internal2 (
+    Parser,
+    pToken,
+    pKeyword,
+    pControl,
+    pSuccess,
+    tryConsume,
+    tryParse,
+    pSepBy,
+    pCommaSep,
+) where
+
+import Data.Void (Void)
+import Lexer.Tokens (Token (Keyword, Control), Keyword, ControlSequence (Comma))
+import Lexer (WithPos(WithPos))
+import Text.Megaparsec (Parsec, initialPos, MonadParsec (token), ErrorItem (Tokens), many, parseMaybe)
+import AlexToParsec (TokenStream)
+import Data.List.NonEmpty(NonEmpty(..))
+import qualified Data.Set as Set
+import Control.Applicative((<|>))
+import Data.Functor((<&>), void)
+import Data.Maybe (maybeToList)
+
+type Parser = Parsec Void TokenStream
+
+pSuccess :: Parser ()
+pSuccess = return ()
+
+tryConsume :: Parser a -> Parser ()
+tryConsume p = void p <|> return ()
+
+tryParse :: Parser a -> Parser (Maybe a)
+tryParse p = (p <&> Just) <|> return Nothing
+
+liftMyToken :: Token -> WithPos Token
+liftMyToken = WithPos pos pos 0
+  where
+    pos = initialPos ""
+
+pToken :: Token -> Parser Token
+pToken c = token test (Set.singleton . Tokens . nes . liftMyToken $ c)
+  where
+    test (WithPos _ _ _ x) =
+      if x == c
+        then Just x
+        else Nothing
+    nes x = x :| []
+
+pKeyword :: Keyword -> Parser Token
+pKeyword = pToken . Keyword
+
+pControl :: ControlSequence -> Parser Token
+pControl = pToken . Control
+
+-- Powerful combinator which parses separators and elements,
+-- returning the list of elements.
+--
+-- It allows both leading and trailing separators, as well as empty lists.
+-- The return value of the separator parser is discarded.
+pSepBy :: Parser a -> Parser b -> Parser [b]
+pSepBy sep p = do
+    x <- tryParse p        -- Attempt to parse a leading expr
+    xs <- many (sep >> p)  -- Parse all <sep expr>
+    tryConsume sep         -- Ignore potential trailing separator
+
+    return (maybeToList x ++ xs)
+
+pCommaSep :: Parser a -> Parser [a]
+pCommaSep = pSepBy (pControl Comma)
---
+++ src/Parser2.hs
@@ -0,0 +1,49 @@
+module Parser2 (
+) where
+
+import qualified Data.Set as Set
+import Text.Megaparsec (
+    (<?>),
+    token, choice,
+    )
+
+import Data.Functor((<&>), ($>))
+import Control.Applicative((<|>))
+
+import Parser.Internal2
+
+import Lexer.Tokens (Literal(..), Token(..), Keyword (..))
+import Lexer (WithPos(WithPos))
+import Data.Int (Int64)
+import Data.Text (Text)
+import Parser.AST (VarIdentifier (VarIdentifier), TypeIdentifier (TypeIdentifier), PrimitiveExpression (PrimIntLiteral, PrimIdentifier, PrimBooleanLiteral))
+
+pIntLiteral :: Parser Int64
+pIntLiteral = token test Set.empty <?> "integer literal"
+    where
+        test (WithPos _ _ _ (Literal (IntLiteral n))) = Just n
+        test _ = Nothing
+
+pIdentifier :: Parser Text
+pIdentifier = token test Set.empty <?> "identifier"
+    where
+        test (WithPos _ _ _ (Identifier i)) = Just i
+        test _ = Nothing
+
+pVarIdentifier :: Parser VarIdentifier
+pVarIdentifier = pIdentifier <&> VarIdentifier <?> "variable identifier"
+
+pTypeIdentifier :: Parser TypeIdentifier
+pTypeIdentifier = pIdentifier <&> TypeIdentifier <?> "type identifier"
+
+pBoolean :: Parser Bool
+pBoolean = (pKeyword KeyWTrue $> True
+        <|> pKeyword KeyWFalse $> False)
+        <?> "boolean literal"
+
+pPrimitive :: Parser PrimitiveExpression
+pPrimitive = choice
+    [ pIntLiteral       <&> PrimIntLiteral
+    , pVarIdentifier    <&> PrimIdentifier
+    , pBoolean          <&> PrimBooleanLiteral
+    ]
