9ec8440f305ee7a2314806edf9c18fd51a98f5bd
Florent Charpentier <florent.charpentier@epitech.eu>
2024-12-30 23:52:44 +0100

    feat(lexer): Make `Token` instance of `Ord`

--- src/Lexer/Tokens.hs
+++ src/Lexer/Tokens.hs
@@ -27,12 +27,12 @@ data Token
     | Literal Literal
     | Identifier Text
     | Control ControlSequence
-    deriving (Show, Eq)
+    deriving (Show, Eq, Ord)
 
 -- we use 'newtype' instead of data because we only have one variant so far
 newtype Literal
     = IntLiteral Int64
-    deriving (Show, Eq)
+    deriving (Show, Eq, Ord)
 
 data Keyword
     = KeyWReturn
@@ -43,7 +43,7 @@ data Keyword
     | KeyWTrue
     | KeyWFalse
     | KeyWMain
-    deriving (Show, Eq)
+    deriving (Show, Eq, Ord)
 
 data ControlSequence
     = LineBreak
@@ -69,4 +69,4 @@ data ControlSequence
     | OperLt
     | OperGe
     | OperLe
-    deriving (Show, Eq)
+    deriving (Show, Eq, Ord)
