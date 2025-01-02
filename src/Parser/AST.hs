60f077edc4ea089466e4da91d994fdf0a02ad29a
Florent Charpentier <florent.charpentier@epitech.eu>
2025-01-02 13:26:51 +0100

    feat: Make all AST data structures instances of Eq and Show

--- src/Parser/AST.hs
+++ src/Parser/AST.hs
@@ -20,16 +20,21 @@ import Data.Text (Text)
 import Data.Int (Int64)
 
 data Program = Program MainFunction [Function]
+    deriving (Eq, Show)
 
 data MainFunction = MainFunction [VariableDeclaration] BlockExpression
+    deriving (Eq, Show)
 data Function = Function Text [VariableDeclaration] (Maybe TypeIdentifier) BlockExpression
+    deriving (Eq, Show)
 
 newtype BlockExpression = BlockExpression [Statement]
+    deriving (Eq, Show)
 
 data Statement
     = StExpression Expression
     | StVariableDecl VariableDeclaration (Maybe Expression)
     | StReturn Expression
+    deriving (Eq, Show)
 
 data Expression
     = ExprIfConditional Expression Expression (Maybe Expression)
@@ -37,15 +42,18 @@ data Expression
     | ExprOperation Operation
     | ExprBlock BlockExpression
     | ExprAtomic AtomicExpression
+    deriving (Eq, Show)
 
 data Operation
     = OpPrefix PrefixOperation
     | OpInfix  InfixOperation
+    deriving (Eq, Show)
 
 data PrefixOperation
     = PreNot  Expression
     | PrePlus Expression
     | PreNeg  Expression
+    deriving (Eq, Show)
 type PrefixOperator = (Expression -> PrefixOperation)
 
 data InfixOperation
@@ -62,14 +70,19 @@ data InfixOperation
     | InfixLe   Expression Expression
     | InfixAnd  Expression Expression
     | InfixOr   Expression Expression
+    deriving (Eq, Show)
 type InfixOperator = (Expression -> Expression -> InfixOperation)
 
 data AtomicExpression
     = AtomIntLiteral Int64
     | AtomBooleanLiteral Bool
     | AtomIdentifier VarIdentifier
+    deriving (Eq, Show)
 
 newtype VarIdentifier = VarIdentifier Text
+    deriving (Eq, Show)
 newtype TypeIdentifier = TypeIdentifier Text
+    deriving (Eq, Show)
 
 data VariableDeclaration = VariableDeclaration TypeIdentifier VarIdentifier
+    deriving (Eq, Show)
