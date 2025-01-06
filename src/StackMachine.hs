2c470ed40669ec3d031452367b057d04e00f11db
flobath <florian.gaudin@epitech.eu>
2025-01-06 15:12:03 +0100

    [FIX] Error handling

--- src/StackMachine.hs
+++ src/StackMachine.hs
@@ -51,25 +51,25 @@ type Environment = [(String, Value)]
 push :: Value -> Stack -> Stack
 push x stack = x : stack
 
-pop :: Stack -> (Value, Stack)
-pop (x:xs) = (x, xs)
-pop [] = error "Cannot pop from an empty stack"
+pop :: Stack -> Either String (Value, Stack)
+pop (x:xs) = Right (x, xs)
+pop [] = Left "Cannot pop empty stack"
 
-pushArgs :: Int -> Args -> Stack -> Stack
+pushArgs :: Int -> Args -> Stack -> Either String Stack
 pushArgs n args stack = case (n, args) of
-    (0, (x:_)) -> push x stack
+    (0, (x:_)) -> Right (push x stack)
     (a, (_:xs)) -> pushArgs (a-1) xs stack
-    (_, []) -> error "Arguments index out of bounds"
+    (_, []) -> Left "Not enough arguments"
 
-pushEnv :: String -> Environment -> Stack -> Stack
+pushEnv :: String -> Environment -> Stack -> Either String Stack
 pushEnv name env stack = case lookup name env of
 
-    Just value -> push value stack
-    Nothing -> error "Environment variable not found"
+    Just value -> Right (push value stack)
+    Nothing -> Left "Cannot find value in environment"
 
 -- Arithmetic operators
 
-applyOperator :: Operator -> Stack -> Stack
+applyOperator :: Operator -> Stack -> Either String Stack
 applyOperator Add = addition
 applyOperator Sub = subtraction
 applyOperator Mul = multiplication
@@ -84,63 +84,63 @@ applyOperator Ge = greaterEqual
 applyOperator And = andOperator
 applyOperator Or = orOperator
 
-addition :: Stack -> Stack
-addition (IntValue x : IntValue y : xs) = IntValue (x + y) : xs
-addition _ = error "Cannot apply addition"
+addition :: Stack -> Either String Stack
+addition (IntValue x : IntValue y : xs) = Right (IntValue (x + y) : xs)
+addition _ = Left "Addition failed"
 
-subtraction :: Stack -> Stack
-subtraction (IntValue x : IntValue y : xs) = IntValue (x - y) : xs
-subtraction _ = error "Cannot apply subtraction"
+subtraction :: Stack -> Either String Stack
+subtraction (IntValue x : IntValue y : xs) = Right (IntValue (x - y) : xs)
+subtraction _ = Left "Cannot apply subtraction"
 
-multiplication :: Stack -> Stack
-multiplication (IntValue x : IntValue y : xs) = IntValue (x * y) : xs
-multiplication _ = error "Cannot apply multiplication"
+multiplication :: Stack -> Either String Stack
+multiplication (IntValue x : IntValue y : xs) = Right (IntValue (x * y) : xs)
+multiplication _ = Left "Cannot apply multiplication"
 
-division :: Stack -> Stack
-division (IntValue _ : IntValue 0 : _) = error "Division by zero"
-division (IntValue x : IntValue y : xs) = IntValue (x `div` y) : xs
-division _ = error "Cannot apply division"
+division :: Stack -> Either String Stack
+division (IntValue _ : IntValue 0 : _) = Left "Division by zero"
+division (IntValue x : IntValue y : xs) = Right (IntValue (x `div` y) : xs)
+division _ = Left "Cannot apply division"
 
-modulus :: Stack -> Stack
-modulus (IntValue _ : IntValue 0 : _) = error "Modulus by zero"
-modulus (IntValue x : IntValue y : xs) = IntValue (x `mod` y) : xs
-modulus _ = error "Cannot apply modulus"
+modulus :: Stack -> Either String Stack
+modulus (IntValue _ : IntValue 0 : _) = Left "Modulus by zero"
+modulus (IntValue x : IntValue y : xs) = Right (IntValue (x `mod` y) : xs)
+modulus _ = Left "Cannot apply modulus"
 
 -- Comparison operators
 
-equal :: Stack -> Stack
-equal (IntValue x : IntValue y : xs) = BoolValue (x == y) : xs
-equal (BoolValue x : BoolValue y : xs) = BoolValue (x == y) : xs
-equal _ = error "Cannot apply equal"
+equal :: Stack -> Either String Stack
+equal (IntValue x : IntValue y : xs) = Right (BoolValue (x == y) : xs)
+equal (BoolValue x : BoolValue y : xs) = Right (BoolValue (x == y) : xs)
+equal _ = Left "Cannot apply equal"
 
-notEqual :: Stack -> Stack
-notEqual (IntValue x : IntValue y : xs) = BoolValue (x /= y) : xs
-notEqual (BoolValue x : BoolValue y : xs) = BoolValue (x /= y) : xs
-notEqual _ = error "Cannot apply not equal"
+notEqual :: Stack -> Either String Stack
+notEqual (IntValue x : IntValue y : xs) = Right (BoolValue (x /= y) : xs)
+notEqual (BoolValue x : BoolValue y : xs) = Right (BoolValue (x /= y) : xs)
+notEqual _ = Left "Cannot apply not equal"
 
-lessThan :: Stack -> Stack
-lessThan (IntValue x : IntValue y : xs) = BoolValue (x < y) : xs
-lessThan _ = error "Cannot apply less than"
+lessThan :: Stack -> Either String Stack
+lessThan (IntValue x : IntValue y : xs) = Right (BoolValue (x < y) : xs)
+lessThan _ = Left "Cannot apply less than"
 
-lessEqual :: Stack -> Stack
-lessEqual (IntValue x : IntValue y : xs) = BoolValue (x <= y) : xs
-lessEqual _ = error "Cannot apply less equal"
+lessEqual :: Stack -> Either String Stack
+lessEqual (IntValue x : IntValue y : xs) = Right (BoolValue (x <= y) : xs)
+lessEqual _ = Left "Cannot apply less equal"
 
-greaterThan :: Stack -> Stack
-greaterThan (IntValue x : IntValue y : xs) = BoolValue (x > y) : xs
-greaterThan _ = error "Cannot apply greater than"
+greaterThan :: Stack -> Either String Stack
+greaterThan (IntValue x : IntValue y : xs) = Right (BoolValue (x > y) : xs)
+greaterThan _ = Left "Cannot apply greater than"
 
-greaterEqual :: Stack -> Stack
-greaterEqual (IntValue x : IntValue y : xs) = BoolValue (x >= y) : xs
-greaterEqual _ = error "Cannot apply greater equal"
+greaterEqual :: Stack -> Either String Stack
+greaterEqual (IntValue x : IntValue y : xs) = Right (BoolValue (x >= y) : xs)
+greaterEqual _ = Left "Cannot apply greater equal"
 
-andOperator :: Stack -> Stack
-andOperator (BoolValue x : BoolValue y : xs) = BoolValue (x && y) : xs
-andOperator _ = error "Cannot apply and"
+andOperator :: Stack -> Either String Stack
+andOperator (BoolValue x : BoolValue y : xs) = Right (BoolValue (x && y) : xs)
+andOperator _ = Left "Cannot apply and"
 
-orOperator :: Stack -> Stack
-orOperator (BoolValue x : BoolValue y : xs) = BoolValue (x || y) : xs
-orOperator _ = error "Cannot apply or"
+orOperator :: Stack -> Either String Stack
+orOperator (BoolValue x : BoolValue y : xs) = Right (BoolValue (x || y) : xs)
+orOperator _ = Left "Cannot apply or"
 
 -- Control flow
 
@@ -154,18 +154,32 @@ jumpIfFalse _ _ program = program
 
 -- Execution
 
-execute :: Environment -> Args -> Program -> Stack -> Value
-execute _ _ [] stack = let (result, _) = pop stack in result
+getValueFromPop :: Either String (Value, Stack) -> Either String Value
+getValueFromPop (Right (value, _)) = Right value
+getValueFromPop (Left error) = Left error
+
+execute :: Environment -> Args -> Program -> Stack -> Either String Value
+-- execute _ _ [] stack = let (result, _) = case pop stack of
+--         Right (value, _) -> (Right value)
+--         Left err -> Left err
 execute env args (Return : _) stack = execute env args [] stack
 
 execute env args (Push x : prog') stack = execute env args prog' (push x stack)
-execute env args ((PushArgs n) : prog') stack = execute env args prog' (pushArgs n args stack)
-execute env args ((PushEnv name) : prog') stack = execute env args prog' (pushEnv name env stack)
+execute env args ((PushArgs n) : prog') stack = case pushArgs n args stack of
+    Right stack' -> execute env args prog' stack'
+    Left err -> Left err
+execute env args ((PushEnv name) : prog') stack = case pushEnv name env stack of
+    Right stack' -> execute env args prog' stack'
+    Left err -> Left err
 
 execute env args (Call : prog') stack = case stack of
-    (FuncValue func : stack') -> execute env args prog' (execute env stack' func [] : stack')
-    _ -> error "Cannot call"
-execute env args ((OpValue op) : prog') stack = execute env args prog' (applyOperator op stack)
+    (FuncValue func : stack') -> case execute env stack' func [] of
+        Left err -> Left err
+        Right value -> execute env args prog' (value : stack')
+    _ -> Left "Cannot call"
+execute env args ((OpValue op) : prog') stack = case applyOperator op stack of
+    Left err -> Left err
+    Right stack' -> execute env args prog' stack'
 
-execute env args ((JumpIfTrue n) : prog') stack = let (bool, stack') = pop stack in execute env args (jumpIfTrue n (bool:stack') prog') stack'
-execute env args ((JumpIfFalse n) : prog') stack = let (bool, stack') = pop stack in execute env args (jumpIfFalse n (bool:stack') prog') stack'
+execute env args (JumpIfTrue n : prog') stack = execute env args (jumpIfTrue n stack prog') stack
+execute env args (JumpIfFalse n : prog') stack = execute env args (jumpIfFalse n stack prog') stack
