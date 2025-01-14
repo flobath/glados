507f32c6d762f49efd8542c623c791e437cf9b8f
Thibaut Hebert--Henriette <thibaut.hebert-henriette@epitech.eu>
2025-01-14 14:51:55 +0100

    test(compiler): Add full-program test with conditions

--- test/CompilerSpec.hs
+++ test/CompilerSpec.hs
@@ -25,6 +25,7 @@ localIntDecl name x = StVariableDecl (VariableDeclaration (typeId "i32") $ varId
 programA = Program (MainFunction [] (BlockExpression [StReturn $ sumExpr (intConstant 3) (intConstant 5)])) []
 programB = Program (MainFunction [] (BlockExpression [localIntDecl "a" 5, StReturn (varRef "a")])) []
 programC = Program (MainFunction [] (BlockExpression [localIntDecl "a" 5, StReturn (ExprOperation (OpInfix (InfixAdd (intConstant 5) (varRef "a"))))])) []
+programD = Program (MainFunction [] (BlockExpression [localIntDecl "a" 5, StReturn (ExprIfConditional (ExprOperation (OpInfix (InfixGt (intConstant 10) (varRef "a")))) (intConstant 0) $ Just (intConstant 1))])) []
 
 spec :: Spec
 spec = do
@@ -35,3 +36,5 @@ spec = do
             convertToStackInstructions programB `shouldBe` Right [PushValue (IntValue 5), StoreEnv "a", PushEnv "a", Return]
         it "Simple variable addition return" $ do
             convertToStackInstructions programC `shouldBe` Right [PushValue (IntValue 5), StoreEnv "a", PushValue (IntValue 5), PushEnv "a", OpValue Add, Return]
+        it "Simple conditional return" $ do
+            convertToStackInstructions programD `shouldBe` Right [PushValue (IntValue 5), StoreEnv "a", PushValue (IntValue 10), PushEnv "a", OpValue Gt, JumpIfFalse 2, PushValue (IntValue 0), Jump 1, PushValue (IntValue 1), Return]
