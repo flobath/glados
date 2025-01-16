07598d9a8ce88db9b51cd714d367e50a8d9cbb48
Florent Charpentier <florent.charpentier@epitech.eu>
2025-01-16 20:34:18 +0100

    feat: Add basic disassembler module

--- src/Disassembly.hs
+++ src/Disassembly.hs
@@ -1,5 +1,10 @@
-module Disassembly (disassemble) where
+module Disassembly (disassemble, disassembleFile) where
 import StackMachine (StackProgram)
+
+disassembleFile :: (FilePath, StackProgram) -> String
+disassembleFile (file, stack) = "\n"
+    ++ file ++ ":\n"
+    ++ unlines (map (("  " ++) . show) stack)
 
 disassemble :: StackProgram -> String
 disassemble = unlines . map show
