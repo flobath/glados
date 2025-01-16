cfac49b00c63ac0cfadf27d083b4b8b92c9412e4
Florent Charpentier <florent.charpentier@epitech.eu>
2025-01-16 14:48:09 +0100

    refactor: Move VM/compiler logic out of main function

--- app/Main-exe.hs
+++ app/Main-exe.hs
@@ -1,19 +1,9 @@
 {-# OPTIONS_GHC -Wno-missing-signatures #-}
-{-# LANGUAGE OverloadedStrings #-}
 
 module Main (main) where
 
-import GldsBytecode(readProgramFromFile)
-import StackMachine (execute')
 import System.Environment (getArgs)
-import Helpers (headOr, orExitWith, exitWithErrorMessage, orelse)
+import VM(vm)
 
 main :: IO ()
-main = do
-    args <- getArgs
-    file <- headOr args `orExitWith` "No program to execute"
-    eitherProgram <- readProgramFromFile file
-    program <- eitherProgram `orelse` exitWithErrorMessage
-    result <- execute' program `orelse` exitWithErrorMessage
-
-    print result
+main = vm =<< getArgs
