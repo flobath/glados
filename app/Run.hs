601624aaa62dbdf7724e76a6a9e24b9af02a621b
Florent Charpentier <florent.charpentier@epitech.eu>
2025-01-19 19:39:42 +0100

    feat(run): Allow running from standard input

--- app/Run.hs
+++ app/Run.hs
@@ -1,14 +1,29 @@
 module Run (run) where
 import Helpers (exitWithErrorMessage, orelse)
+import Data.Text(Text)
 import qualified Data.Text.IO as T.IO
 import Compiler (compileFile)
 import StackMachine (execute')
+import System.Exit (exitSuccess)
 
+runHelpMessage :: String
+runHelpMessage  = "Usage: run {filename | -}\n\n"
+                ++ "Run a FunChill program directly from source code\n"
+                ++ "Input can be a file, or stdin with \"-\" as filename\n"
 
-run :: [String] -> IO ()
-run [inputFile] = do
-    contents <- T.IO.readFile inputFile
+runFromContent :: Text -> IO ()
+runFromContent contents = do
     program <- compileFile contents `orelse` exitWithErrorMessage
     result <- execute' program `orelse` exitWithErrorMessage
     print result
-run _ = exitWithErrorMessage "Expecting a single file to run"
+
+run :: [String] -> IO ()
+run ["--help"] = putStrLn runHelpMessage >> exitSuccess
+run ["-h"] = run ["--help"]
+run ["-"] = do
+    contents <- T.IO.getContents
+    runFromContent contents
+run [inputFile] = do
+    contents <- T.IO.readFile inputFile
+    runFromContent contents
+run _ = exitWithErrorMessage runHelpMessage
